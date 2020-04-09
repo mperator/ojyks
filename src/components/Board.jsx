775e0fcd5890adb27e5c8c9ff88721d5c551681e
Patrick Mutter <develop@mperia.de>
2020-04-09 18:27:50 +0200

    Add css for gamefield.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,10 +8,30 @@ export default function Board(props) {
     return (
         <div className="board">
             {props.cards.map((card, i) => 
-                card ? 
-                <Card card={card} key={i} cell={i} handleClick={props.handleClick}/> : 
-                <div>-</div>
+                <div className="board-card-place">
+                    {card && <Card card={card} key={i} cell={i} handleClick={props.handleClick}/>}
+                </div>
             )}
+
+            {/* <div className="board-card-place">1</div>
+            <div className="board-card-place">2</div>
+            <div className="board-card-place">3</div>
+            <div className="board-card-place">4</div>
+            <div className="board-card-place">5</div>
+            <div className="board-card-place">6</div>
+            <div className="board-card-place">7</div>
+            <div className="board-card-place">8</div>
+            <div className="board-card-place">9</div>
+            <div className="board-card-place">9</div>
+            <div className="board-card-place">9</div>
+            <div className="board-card-place">9</div> */}
         </div>
+        // <div className="board">
+        //     {props.cards.map((card, i) => 
+        //         card ? 
+        //         <Card card={card} key={i} cell={i} handleClick={props.handleClick}/> : 
+        //         <div>-</div>
+        //     )}
+        // </div>
     )
 }
