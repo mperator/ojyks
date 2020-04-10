85e49029e2ef5be6b72e6b6885077e07c73417cc
Patrick Mutter <develop@mperia.de>
2020-04-10 11:48:01 +0200

    Update lobby.

--- client/src/components/Lobby/CreateLobby.jsx
+++ client/src/components/Lobby/CreateLobby.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default class CreateLobby extends Component {
     render() {
         return (
             <div>
                 <h1>create</h1>
+                <input type="text" name="roomname" id="roomname" value="" />
+
+                <button>
+                    <NavLink to="/lobby">create</NavLink>
+                </button>
             </div>
         )
     }
