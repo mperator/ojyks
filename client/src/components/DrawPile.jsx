47c6b24cf39638f296403ba3dabed49715467664
Benjamin Sautermeister <bsautermeister@live.de>
2020-04-22 22:22:03 +0200

    Use animated cards using a sprite sheet (#5)

--- client/src/components/DrawPile.jsx
+++ client/src/components/DrawPile.jsx
@@ -4,9 +4,9 @@ import './DrawPile.css'
 
 export default function DrawPile(props) {
     return (
-        <div className="draw-pile">
+        <div>
             {props.cards.length > 0 && (
-                <Card card={props.cards[0]} cell={-1} handleClick={props.handleClick} source="draw"/>
+                <Card card={props.cards[0]} cardIndex={-1} handleClick={props.handleClick} source="draw"/>
             )}
         </div>
     )
